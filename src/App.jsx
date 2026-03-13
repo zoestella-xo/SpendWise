743db5ba3464db75acd199d147655b8adca66771
zoestella-xo <zoestella.xo@gmail.com>
2026-03-13 04:15:28 -0700

    transaction state has been added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Dashboard from "./Components/Dashboard";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Add from "./Components/Add";
+import { useState } from "react";
 
 
 export default function App() {
+  const [transactions, setTransactions] = useState([]);
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/add" element={<Add />} />
+        <Route path="/" element={<Dashboard transactions={transactions} />} />
+        <Route path="/add" element={<Add setTransactions={setTransactions} />} />
       </Routes>
       <Footer />
     </>
